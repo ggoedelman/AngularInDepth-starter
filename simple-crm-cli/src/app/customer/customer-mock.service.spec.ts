43e96b965af3c1785fb0bdc8cb572d0a88e7b973
Greg Goedelman <ggoedelman@gmail.com>
2022-10-21 15:08:50 -0500

    69: fix service tests

--- simple-crm-cli/src/app/customer/customer-mock.service.spec.ts
+++ simple-crm-cli/src/app/customer/customer-mock.service.spec.ts
@@ -1,16 +1,74 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CustomerMockService } from './customer-mock.service';
+import { Customer } from './customer.model';
 
 describe('CustomerMockService', () => {
   let service: CustomerMockService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CustomerMockService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be John', (done) => {
+    service.get(1).subscribe({
+      next: (cust: Customer) => {
+        expect(cust.firstName).toEqual('John');
+        done();
+      }
+    });
+  });
+
+  it('should be Smith', (done) => {
+    service.get(1).subscribe({
+      next: (cust: Customer) => {
+        expect(cust.lastName).toEqual('Smith');
+        done();
+      }
+    });
+  });
+
+  it('should be phone', (done) => {
+    service.get(1).subscribe({
+      next: (cust: Customer) => {
+        expect(cust.preferredContactMethod).toEqual('phone');
+        done();
+      }
+    });
+  });
+
+  it('should be Bob', (done) => {
+    service.get(2).subscribe({
+      next: (cust: Customer) => {
+        expect(cust.firstName).toEqual('Bob');
+        done();
+      }
+    });
+  });
+
+  it('should be Jones', (done) => {
+    service.get(2).subscribe({
+      next: (cust: Customer) => {
+        expect(cust.lastName).toEqual('Jones');
+        done();
+      }
+    });
+  });
+
+  it('should be email', (done) => {
+    service.get(2).subscribe({
+      next: (cust: Customer) => {
+        expect(cust.preferredContactMethod).toEqual('email');
+        done();
+      }
+    });
   });
 });
--- simple-crm-cli/src/app/customer/customer.service.spec.ts
+++ simple-crm-cli/src/app/customer/customer.service.spec.ts
@@ -1,16 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+  import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { CustomerService } from './customer.service';
+  import { CustomerService } from './customer.service';
 
-describe('CustomerService', () => {
-  let service: CustomerService;
+  describe('CustomerService', () => {
+    let injector: TestBed;
+    let service: CustomerService;
+    let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CustomerService);
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [ HttpClientTestingModule ],
+        providers: [ CustomerService ]
+      });
+      injector = getTestBed();
+      service = injector.inject(CustomerService);
+      httpMock = injector.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+      httpMock.verify(); // ensures there are no outstanding requests between tests.
+    });
+
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
   });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
