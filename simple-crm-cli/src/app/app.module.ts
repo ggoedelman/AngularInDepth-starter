501f858a26713a3bb78d3b38ca6a10d44524e0d5
Greg Goedelman <ggoedelman@gmail.com>
2022-11-09 11:17:58 -0600

    163: converted to a PWA

--- simple-crm-cli/src/app/app.module.ts
+++ simple-crm-cli/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { AccountModule } from './account/account.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AccountInterceptor } from './account/account-interceptor';
 import { AccountService } from './account/account.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,13 @@ import { AccountService } from './account/account.service';
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     EffectsModule.forRoot([]),
     AccountModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {
